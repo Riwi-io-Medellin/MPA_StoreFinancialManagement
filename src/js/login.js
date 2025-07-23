1296a551a2eb449985bce6b60c11278ab976d76a
Javier Cómbita Téllez <javiercombita2014@gmail.com>
2025-07-23 15:23:36 -0500

    feat: the login system with local storage was created

--- src/js/login.js
+++ src/js/login.js
@@ -1,9 +1,33 @@
 const formLogin = document.getElementById("form-login")
 
-formLogin.addEventListener("submit",function (event) {
+formLogin.addEventListener("submit", function (event) {
 
-    console.log(formLogin.username.value);
-    console.log(formLogin.password.value);
+    const inputUsername = formLogin.username.value
+    const inputPassword = formLogin.password.value
+    login(inputUsername, inputPassword)
 
     event.preventDefault()
 })
+
+
+async function login(inputUsername, inputPassword) {
+    let response = await fetch(`http://localhost:3000/users?username=${inputUsername}`)
+    let data = await response.json()
+
+    if (data.length === 0) {
+        alert("credenciales incorrectas, revisa el correo o la contraseña")
+    } else {
+        const userFound = data[0]
+
+        if (userFound.password === inputPassword) {
+
+            localStorage.setItem("currentUser", JSON.stringify(userFound))
+
+            alert("login correcto")
+        } else {
+            alert("credenciales incorrectas, revisa el correo o la contraseña")
+        }
+    }
+}
+
+
